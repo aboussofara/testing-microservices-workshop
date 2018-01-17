4c5eae08ba801f4f303b59b49f3b72364eaf5e4b
Sina Jahan <sjahangi@thoughtworks.com>
2018-01-17 16:27:01 -0500

    first try to get pact

--- topics/finish/contract-testing/shop/test.integration.js
+++ topics/finish/contract-testing/shop/test.integration.js
@@ -1,55 +1,68 @@
-let chai = require('chai');
-let expect = chai.expect;
-let request = require('request-promise-native');
-let shop = require('./shop');
+const chai = require('chai')
+const path = require('path')
+const chaiAsPromised = require('chai-as-promised')
+const pact = require('pact')
+const expect = chai.expect
+const API_PORT = process.env.API_PORT || 9081
+const {
+  shop
+} = require('../shop')
+chai.use(chaiAsPromised)
 
-describe('shop', () => {
-  before((done) => {
-    // import the mountebank helper library
-    const mb = require('mountebank-helper');
+// Configure and import consumer API
+// Note that we update the API endpoint to point at the Mock Service
+const LOG_LEVEL = process.env.LOG_LEVEL || 'WARN'
 
-    // create the skeleton for the imposter (does not post to MB)
-    const catalogueImposter = new mb.Imposter({ 'imposterPort' : 9081 });
-    const reviewImposter = new mb.Imposter({ 'imposterPort' : 9082 });
+const provider = pact({
+  consumer: 'shop',
+  provider: 'catalogue',
+  port: API_PORT,
+  log: path.resolve(process.cwd(), 'logs', 'pact.log'),
+  dir: path.resolve(process.cwd(), 'pacts'),
+  logLevel: LOG_LEVEL,
+  spec: 2
+})
+const expectedBody = {
+  sku: 1
+}
 
-    const catalogueResponse = {
-      'uri' : '/products',
-      'verb' : 'GET',
-      'res' : {
-        'statusCode': 200,
-        'responseHeaders' : { 'Content-Type' : 'application/json' },
-        'responseBody' : JSON.stringify([{ sku: 1 }])
-      }
-    };
-
-    const reviewResponse = {
-      'uri' : '/reviews',
-      'verb' : 'GET',
-      'res' : {
-        'statusCode': 200,
-        'responseHeaders' : { 'Content-Type' : 'application/json' },
-        'responseBody' : JSON.stringify({ 1: [{}] })
-      }
-    };
-
-    catalogueImposter.addRoute(catalogueResponse);
-    reviewImposter.addRoute(reviewResponse);
-
-    mb.startMbServer(2525)
-      .then(function() {
-        Promise.all([
-          catalogueImposter.postToMountebank(),
-          reviewImposter.postToMountebank(),
-        ])
-        .then( () => {
-          done();
-        })
+describe('Pact with catalogue', () => {
+  //describe('given data count > 0', () => {
+    describe('when a call to the Provider is made', () => {
+      before(() => {
+        return provider.setup()
+          .then(() => {
+            provider.addInteraction({
+              uponReceiving: 'a request for JSON data',
+              withRequest: {
+                method: 'GET',
+                path: '/products'
+              },
+              willRespondWith: {
+                status: 200,
+                headers: {
+                  'Content-Type': 'application/json; charset=utf-8'
+                },
+                body: expectedBody
+              }
+            })
+          })
       })
-  });
 
-  it('returns products with reviews', () => {
-    return shop().then((products) => {
-      expect(products.length).to.equal(1);
+      it('can process the JSON payload from the provider', () => {
+        const response = shop()
+
+        return expect(response).to.eventually.have.property('sku', 0.1)
+      })
+
+      it('should validate the interactions and create a contract', () => {
+        return provider.verify()
+      })
     })
-  })
-});
+
+    // Write pact files to file
+    after(() => {
+      return provider.finalize()
+    })
+//  })
+})
