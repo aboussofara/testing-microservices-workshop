85d6606b7ce5b473cea02d3ab4da94954e7a7526
Sina Jahan <sjahangi@thoughtworks.com>
2018-02-05 21:24:46 -0500

    add docker-compose for 01-getting started

--- topics/start/01-getting-started/shop/test.js
+++ topics/start/01-getting-started/shop/test.js
@@ -3,14 +3,22 @@ let expect = chai.expect;
 var nock = require('nock');
 
 describe('shop', () => {
-  it('returns products with reviews', () => {
-
+  it('returns sorted products alphabetically', () => {
     var scope = nock('http://localhost:9081')
       .get('/products')
-      .reply(200, [{"sku":1,"title":"Flood Light with Cable and Plug LED","color":"red"}]);
+      .reply(200, [
+        { 'sku': 1, 'title': 'Flood Light with Cable and Plug LED', 'color': 'red' },
+        { 'sku': 2, 'title': 'Crock-Pot SCVT650PS-CN 6.5 Quart', 'color': 'silver' }
+      ]);
 
     let shop = require('./shop');
 
-    return shop().then((products) => { expect(products.length).to.equal(1); } );
+    return shop()
+      .then((products) => {
+        expect(products).to.deep.equal([
+          { 'sku': 2, 'title': 'Crock-Pot SCVT650PS-CN 6.5 Quart', 'color': 'silver' },
+          { 'sku': 1, 'title': 'Flood Light with Cable and Plug LED', 'color': 'red' }
+        ]);
+      });
   })
 });
