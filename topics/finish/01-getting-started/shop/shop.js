9ec3e1e16ebf8710763d576e7babad026d8c14fc
Vivian So <vso@thoughtworks.com>
2018-02-05 21:15:14 -0500

    Vivian & Sina - adding realistic assertion in unit test

--- topics/finish/01-getting-started/shop/shop.js
+++ topics/finish/01-getting-started/shop/shop.js
@@ -7,7 +7,13 @@ module.exports = () => {
   return new Promise((resolve, reject) => {
     request({ uri: `http://${CATALOGUE_HOST}:9081/products`, json: true })
       .then((data) => {
-        resolve(data);
+        resolve(data.sort((first, second) => {
+          if (first.title < second.title)
+            return -1;
+          if (first.title > second.title)
+            return 1;
+          return 0;
+        }));
     })
     .catch(reject);
   });
